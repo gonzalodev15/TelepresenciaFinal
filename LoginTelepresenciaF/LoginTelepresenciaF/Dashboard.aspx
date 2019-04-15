05ae1af9073e9aba46fb33c96a6f85a4f6a3803f
Gonzalo <u201513219@upc.edu.pe>
2019-04-14 21:05:37 -0500

    Login con DB

---
+++ LoginTelepresenciaF/LoginTelepresenciaF/Dashboard.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Dashboard.aspx.cs" Inherits="Dashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblUserDetails" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Button ID="btnLogout" runat="server" Text="Logout" OnClick="btnLogout_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ LoginTelepresenciaF/LoginTelepresenciaF/Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblErrorMessage.Visible = false;
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        using (SqlConnection sqlCon = new SqlConnection(@"Data Source=(local)\sqle2012;initial Catalog=LoginDB;
+        Integrated Security=True"))
+        {
+            sqlCon.Open();
+            string query = "SELECT COUNT(1) From tblUser WHERE username=@username AND password=@password";
+            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+            sqlCmd.Parameters.AddWithValue("@username", txtUsername.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@password", txtPassword.Text.Trim());
+            int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+            if(count == 1)
+            {
+                Session["username"] = txtUsername.Text.Trim();
+                Response.Redirect("Dashboard.aspx");
+            }
+            else { lblErrorMessage.Visible = true; }
+        }
+    }
+}
